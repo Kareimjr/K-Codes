d8d5dc9ea4218481b56fc9ee28ed55a7d5eb3042
Kareimjr <officialkareim@hotmail.com>
2024-08-01 17:41:28 +0100

    .

--- Food-Delivery/frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ Food-Delivery/frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,22 +4,22 @@ import { StoreContext } from '../../context/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
 
 const FoodDisplay = ({ category }) => {
+
     const { food_list } = useContext(StoreContext);
 
     return (
         <div className='food-display' id='food-display'>
             <h2>Top Dishes Near You</h2>
             <div className="food-display-list">
-                {food_list.map((item) => (
-                    <FoodItem
-                        key={item.id}
-                        id={item.id}
-                        name={item.name}
-                        description={item.description}
-                        price={item.price}
-                        image={item.image}
-                    />
-                ))}
+                {food_list.map((item, index) => {
+
+                    if (category === "All" || category===item.category) {
+                        return <FoodItem
+                        key={index} id={item.id} name={item.name} description={item.description} price={item.price} image={item.image} />
+                    }
+
+                    
+                })}
             </div>
         </div>
     );
